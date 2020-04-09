be79c7a96d80d5d8dc783bec6b572758485174db
Nicolas Fraisse <ni.fraisse@gmail.com>
2020-04-09 18:24:43 -0400

    maintenance index page

---
+++ src/pages/_index_temp.js
@@ -0,0 +1,60 @@
+import React from "react"
+import Main from "../layouts/main.js"
+import Container from "../layouts/container.js"
+import { Link } from "gatsby"
+import '../styles/imports.scss'
+import Project from "../components/project.js"
+import Categories from "../components/categories.js"
+
+export default () =>
+
+  <Main>
+
+
+    <Container width='large'>
+      <h1>Recent Work</h1>
+      <Categories />
+      <Project
+      img='bandwizard.png'
+      title='BandWizard'
+      description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ut autem, exercitationem, delectus ipsa praesentium!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos at ex numquam aliquam obcaecati veniam quia necessitatibus id nemo, perspiciatis recusandae eligendi mollitia? Quos officiis cumque, voluptate voluptatibus dicta similique obcaecati laborum tempore deleniti eligendi veritatis tenetur, numquam recusandae non!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, magni, eligendi. Magni.'
+      icons={['javascript.png', 'rails.png']}
+      />
+
+      <Project
+      img='rentmylife.png'
+      imgWidth='440px'
+      title='Rent My Life'
+      description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ut autem, exercitationem, delectus ipsa praesentium!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos at ex numquam aliquam obcaecati veniam quia necessitatibus id nemo, perspiciatis recusandae eligendi mollitia? Quos officiis cumque, voluptate voluptatibus dicta similique obcaecati laborum tempore deleniti eligendi veritatis tenetur, numquam recusandae non!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, magni, eligendi. Magni.'
+      icons={['javascript.png', 'rails.png']}
+      reverse
+      />
+
+      <Project
+      img='billy-social.png'
+      title='Billy.Social'
+      imgWidth='440px'
+      description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ut autem, exercitationem, delectus ipsa praesentium!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos at ex numquam aliquam obcaecati veniam quia necessitatibus id nemo, perspiciatis recusandae eligendi mollitia? Quos officiis cumque, voluptate voluptatibus dicta similique obcaecati laborum tempore deleniti eligendi veritatis tenetur, numquam recusandae non!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, magni, eligendi. Magni.'
+      icons={['wordpress.png', 'rails.png']}
+      />
+
+      <Project
+      img='journalinteret.png'
+      imgWidth='400px'
+      title="Journal L'intéret, HEC Montréal"
+      description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ut autem, exercitationem, delectus ipsa praesentium!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos at ex numquam aliquam obcaecati veniam quia necessitatibus id nemo, perspiciatis recusandae eligendi mollitia? Quos officiis cumque, voluptate voluptatibus dicta similique obcaecati laborum tempore deleniti eligendi veritatis tenetur, numquam recusandae non!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, magni, eligendi. Magni.'
+      icons={['javascript.png', 'wordpress.png']}
+      reverse
+      />
+
+      <Project
+      img='incorpus.png'
+      title='inCorpus'
+      imgWidth='440px'
+      description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ut autem, exercitationem, delectus ipsa praesentium!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos at ex numquam aliquam obcaecati veniam quia necessitatibus id nemo, perspiciatis recusandae eligendi mollitia? Quos officiis cumque, voluptate voluptatibus dicta similique obcaecati laborum tempore deleniti eligendi veritatis tenetur, numquam recusandae non!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, magni, eligendi. Magni.'
+      icons={['react.png', 'gatsby.png']}
+      />
+
+    </Container>
+  </Main>
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,59 +1,32 @@
+
 import React from "react"
 import Main from "../layouts/main.js"
 import Container from "../layouts/container.js"
 import { Link } from "gatsby"
 import '../styles/imports.scss'
-import Project from "../components/project.js"
-import Categories from "../components/categories.js"
 
+import Logo from "../components/logo.js"
 export default () =>
 
-  <Main>
+<div style={{
+  height: '100vh',
+  padding: '0 4em',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  background: 'linear-gradient(135deg, #ECE9E6, white)'
 
-
-    <Container width='large'>
-      <h1>Recent Work</h1>
-      <Categories />
-      <Project
-      img='bandwizard.png'
-      title='BandWizard'
-      description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ut autem, exercitationem, delectus ipsa praesentium!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos at ex numquam aliquam obcaecati veniam quia necessitatibus id nemo, perspiciatis recusandae eligendi mollitia? Quos officiis cumque, voluptate voluptatibus dicta similique obcaecati laborum tempore deleniti eligendi veritatis tenetur, numquam recusandae non!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, magni, eligendi. Magni.'
-      icons={['javascript.png', 'rails.png']}
-      />
-
-      <Project
-      img='rentmylife.png'
-      imgWidth='440px'
-      title='Rent My Life'
-      description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ut autem, exercitationem, delectus ipsa praesentium!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos at ex numquam aliquam obcaecati veniam quia necessitatibus id nemo, perspiciatis recusandae eligendi mollitia? Quos officiis cumque, voluptate voluptatibus dicta similique obcaecati laborum tempore deleniti eligendi veritatis tenetur, numquam recusandae non!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, magni, eligendi. Magni.'
-      icons={['javascript.png', 'rails.png']}
-      reverse
-      />
-
-      <Project
-      img='billy-social.png'
-      title='Billy.Social'
-      imgWidth='440px'
-      description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ut autem, exercitationem, delectus ipsa praesentium!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos at ex numquam aliquam obcaecati veniam quia necessitatibus id nemo, perspiciatis recusandae eligendi mollitia? Quos officiis cumque, voluptate voluptatibus dicta similique obcaecati laborum tempore deleniti eligendi veritatis tenetur, numquam recusandae non!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, magni, eligendi. Magni.'
-      icons={['wordpress.png', 'rails.png']}
-      />
-
-      <Project
-      img='journalinteret.png'
-      imgWidth='400px'
-      title="Journal L'intéret, HEC Montréal"
-      description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ut autem, exercitationem, delectus ipsa praesentium!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos at ex numquam aliquam obcaecati veniam quia necessitatibus id nemo, perspiciatis recusandae eligendi mollitia? Quos officiis cumque, voluptate voluptatibus dicta similique obcaecati laborum tempore deleniti eligendi veritatis tenetur, numquam recusandae non!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, magni, eligendi. Magni.'
-      icons={['javascript.png', 'wordpress.png']}
-      reverse
-      />
-
-      <Project
-      img='incorpus.png'
-      title='inCorpus'
-      imgWidth='440px'
-      description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ut autem, exercitationem, delectus ipsa praesentium!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos at ex numquam aliquam obcaecati veniam quia necessitatibus id nemo, perspiciatis recusandae eligendi mollitia? Quos officiis cumque, voluptate voluptatibus dicta similique obcaecati laborum tempore deleniti eligendi veritatis tenetur, numquam recusandae non!<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, magni, eligendi. Magni.'
-      icons={['react.png', 'gatsby.png']}
-      />
-
-    </Container>
-  </Main>
+}}>
+  <Logo />
+  <p style={{
+    fontFamily: 'inherit',
+    marginTop: 100,
+  }}>
+    <strong>Coming soon</strong>. For now, you can find me <a href="https://www.linkedin.com/in/nicolasfraisse/">here</a> or contact me <a href="mailto:ni.fraisse@gmail.com">here</a>.
+  </p>
+  <p style={{
+    fontFamily: 'inherit',
+    marginTop: 20
+  }}></p>
+</div>
