5f8e74378c1c04b0f77ead6ec8d590553e536370
Nicolas Fraisse <ni.fraisse@gmail.com>
2020-05-05 14:32:04 -0400

    fixed typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Toolkit from "../components/toolkit.js"
 
 export default () =>
 
-  <Mainz>
+  <Main>
     <Container>
 
       <h1>I'm a web developer based in Montreal, building front-end and full-stack projects.</h1>
