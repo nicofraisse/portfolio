49598648f65519dc9fb4b6c552efa298c2fad8d1
Nicolas Fraisse <ni.fraisse@gmail.com>
2020-06-13 21:59:21 -0400

    color scheme implemented in main

--- src/components/separator.js
+++ src/components/separator.js
@@ -6,7 +6,6 @@ const Separator = (props) => {
   return (
     <div className="separator" style={{
       width: props.width,
-      background: props.color,
       marginBottom: props.marginBottom || '20px',
       marginTop: props.marginTop || '-50px',
       transform: props.transform || 'perspective(18px) rotateX(8deg)'
--- src/layouts/main.js
+++ src/layouts/main.js
@@ -5,77 +5,176 @@ import { ThemeProvider, createGlobalStyle } from "styled-components"
 import '../styles/imports.scss'
 import storage from 'local-storage-fallback'
 
+
+const moonBlue = '#323442'
+const moonBlueSecondary = '#464854'
+const dayCream = '#e5e0d6'
+const dayCreamSecondary = '#f5f3ef'
+
+const intenseOrange = '#ea3223'
+const intensePink = '#75187b'
+
+const gradedPink = '#c863ee'
+const gradedBlue = '#5663ee'
+
+const darkText = '#0e0e0e'
+const darkTextSecondary = '#2e2d2b'
+const darkTextThird = '#969696'
+
+const lightText = '#fbfbfb'
+const lightTextSecondary = '#ebebeb'
+const lightTextThird = '#84858e'
+
+const linkBlue = '#9aa2ef'
+const linkBlueActive = '#575ff7'
+
 const GlobalStyle = createGlobalStyle`
+
+//
+// TEXT
+//
+
+.nicolas {
+  color: ${props => props.theme.mode === 'dark' ? lightText : darkText};
+  transition: 0.15s background-color, color;
+}
+
+.fraisse, .logo-description {
+  color: ${props => props.theme.mode === 'dark' ? lightTextSecondary : darkTextSecondary};
+  transition: 0.15s background-color, color;
+}
+
 #header i {
-  color: ${props => props.theme.mode === 'dark' ? '#FBFBFB' : '#0E0E0E'};
+  color: ${props => props.theme.mode === 'dark' ? lightText : darkText};
+}
+
+h1, h2  {
+  color: ${props => props.theme.mode === 'dark' ? darkText : lightText};
+  transition: 0.15s background-color, color;
+}
+
+p, h3, i  {
+  color: ${props => props.theme.mode === 'dark' ? darkTextSecondary : lightTextSecondary};
+  transition: 0.15s background-color, color;
+}
+
+.link, a {
+  color: ${props => props.theme.mode === 'dark' ? linkBlue : linkBlue} !important;
+}
+
+.active-link {
+  background-color: ${props => props.theme.mode === 'dark' ? linkBlueActive : linkBlueActive} !important;
+  color: white !important;
+  &:hover {
+    color: white !important;
+  }
+}
+
+.date-section, .misc-tools > p {
+  color: ${props => props.theme.mode === 'dark' ? lightTextThird : darkTextThird}
+}
+
+//
+// BACKGROUND
+//
+
+.header-background {
+  background-color: ${props => props.theme.mode === 'dark' ? moonBlue : dayCream };
+  transition: 0.15s background-color;
 }
 
 .background-css-animation {
-  background: ${props => props.theme.mode === 'dark' ? 'rgba(49, 52, 67, 1.00)' : 'rgba(229, 224, 214, 1.00)'}
+  background-color: ${props => props.theme.mode === 'dark' ? moonBlue : dayCream};
+  transition: 0.15s background-color;
 }
 
-html, body {
-  // background-color: ${props => props.theme.mode === 'dark' ? 'rgba(50, 52, 66, 1.00)' : 'rgba(252,250,249, 1);'};
-  transition: 0.15s background-color, color;
-}
-
-#header, {
-  color: ${props => props.theme.mode === 'dark' ? 'rgba(14,14,14, 0.95)' : 'rgba(254,253,252, 0.95)'};
-  transition: 0.15s background-color, color;
-}
-
-.arrow i {
-  color:${props => props.theme.mode === 'dark' ? 'rgba(254, 253, 252, 0.35)' : 'rgba(50, 50, 50, 0.3)'}
-}
-
-.header-background {
-    background-color: ${props => props.theme.mode === 'dark' ? 'rgb(50, 52, 66)' : 'rgba(229, 224, 214, 1.00)'};
-    transition: 0.15s background-color, color;
-}
-
-.nicolas, .logo-description {
-  color: ${props => props.theme.mode === 'dark' ? '#FBFBFB' : '#0E0E0E'};
-  transition: 0.15s background-color, color;
-}
-.fraisse {
-  color: ${props => props.theme.mode === 'dark' ? '#EBEBEB' : 'rgba(0,0,0,0.8)'};
-  transition: 0.15s background-color, color;
-}
-
-p, h1, h2, h3, i  {
-  color: ${props => props.theme.mode === 'dark' ? '#0E0E0E' : '#FBFBFB'};
-  transition: 0.15s background-color, color;
-}
-
-.date-section {
-  color: ${props => props.theme.mode === 'dark' ? 'rgba(0, 0, 0, 0.4);' : 'rgba(255, 255, 255, 0.4);'}
-}
-
-.arrow:hover {
-  color: ${props => props.theme.mode === 'dark' ? '#FBFBFB' : '#1C1C1C'};
-  text-shadow: 0px 5px 15px ${props => props.theme.mode === 'dark' ? 'rgba(230,230,230,0.3)' : 'rgba(0,0,0,0.15)'};
-  background-color: none;
-  transition: 0.1s;
-}
 .code-wrapper {
-  background-color: ${props => props.theme.mode === 'dark' ? 'rgba(228, 224, 215, 1.00)' : 'rgba(50, 52, 66, 1.00)'};
+  background-color: ${props => props.theme.mode === 'dark' ? dayCream : moonBlue};
   padding-bottom: 40px;
-  transition: 0.15s background-color, color;
+  transition: 0.15s background-color;
 }
 
 .photography {
-  background-color: ${props => props.theme.mode === 'dark' ? 'rgba(228, 224, 215, 0.3)' : 'rgba(50, 52, 66, 0.9)'};
-  transition: 0.15s background-color, color;
+  background-color: ${props => props.theme.mode === 'dark' ? dayCreamSecondary : moonBlueSecondary};
+  transition: 0.15s background-color;
 }
 
 .about {
-  background-color: ${props => props.theme.mode === 'dark' ? 'rgba(228, 224, 215, 1.00)' : 'rgba(50, 52, 66, 1.00)'};
-  transition: 0.15s background-color, color;
+  background-color: ${props => props.theme.mode === 'dark' ? dayCream : moonBlue};
+  transition: 0.15s background-color;
 }
 #footer {
-  background-color: ${props => props.theme.mode === 'dark' ? 'rgba(228, 224, 215, 1.00)' : 'rgba(50, 52, 66, 1.00)'};
+  background-color: ${props => props.theme.mode === 'dark' ? dayCream : moonBlue};
+  transition: 0.15s background-color;
+}
+.separator {
+  background: ${props => props.theme.mode === 'dark' ? 'linear-gradient(135deg, rgba(200, 99, 238, 0.6), rgba(86, 99, 238, 0.8))' : 'linear-gradient(135deg, rgba(200, 99, 238, 0.6), rgba(86, 99, 238, 0.8))'};
 }
 
+//
+// COMPONENTS
+//
+
+.arrow i {
+  color:${props => props.theme.mode === 'dark' ? lightTextThird : darkTextThird }
+}
+
+.bounce { transition: 0.25s }
+.bounce:hover {
+  text-shadow: 0px 5px 15px ${props => props.theme.mode === 'dark' ? 'rgba(230,230,230,0.3)' : 'rgba(0,0,0,0.15)'};
+}
+
+.btn-more, .btn-more i {
+  color: ${props => props.theme.mode === 'dark' ? lightTextThird : darkTextThird } !important;
+}
+
+.btn-more {
+  border: 1px solid ${props => props.theme.mode === 'dark' ? lightTextThird : darkTextThird };
+}
+
+.btn-more:hover {
+  color: ${props => props.theme.mode === 'dark' ? lightTextThird : moonBlueSecondary } !important;
+  border: 1px solid ${props => props.theme.mode === 'dark' ? lightTextThird : dayCream };
+  background-color: ${props => props.theme.mode === 'dark' ? 'white' : dayCream };
+  box-shadow: 0px 5px 10px ${props => props.theme.mode === 'dark' ? 'rgba(0, 0, 0, 0.1)' : 'rgba(0, 0, 0, 0.3)' };
+}
+.btn-more:hover i{
+  color: ${props => props.theme.mode === 'dark' ? lightTextThird : moonBlueSecondary } !important;
+  transition: 0.15s;
+}
+
+.btn-more:active {
+  box-shadow: 0px 2px 6px ${props => props.theme.mode === 'dark' ? 'rgba(0, 0, 0, 0.2)' : 'rgba(0, 0, 0, 0.6)' };
+}
+
+//
+// FX
+//
+
+.cube__face--front  {
+  background: linear-gradient(159deg, rgba(117, 24, 123, 0.9) 0%, rgba(234, 50, 35, 0.9) 100%);
+  transition: 0.3s;
+}
+.cube__face--right  {
+  background: linear-gradient(159deg, rgba(234, 50, 35, 0.9) 0%, rgba(117, 24, 123, 0.9) 100%);
+  transition: 0.3s;
+}
+.cube__face--back   {
+  background: linear-gradient(159deg, rgba(117, 24, 123, 0.9) 0%, rgba(234, 50, 35, 0.9) 100%);
+  transition: 0.3s;
+}
+.cube__face--left   {
+  background: linear-gradient(159deg, rgba(234, 50, 35, 0.9) 0%, rgba(117, 24, 123, 0.9) 100%);
+  transition: 0.3s;
+}
+.cube__face--top    {
+  background: linear-gradient(159deg, rgba(117, 24, 123, 0.9) 0%, rgba(234, 50, 35, 0.9) 100%);
+  transition: 0.3s;
+}
+.cube__face--bottom {
+  background: linear-gradient(159deg, rgba(234, 50, 35, 0.9) 0%, rgba(117, 24, 123, 0.9) 100%);
+  transition: 0.3s;
+}
 
 `
 
