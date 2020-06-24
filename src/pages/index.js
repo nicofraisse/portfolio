5476b47f019cc70630995587604914c10cd3e254
Nicolas Fraisse <ni.fraisse@gmail.com>
2020-06-24 11:13:28 -0400

    img width

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,8 +23,11 @@ const Index = () => {
         <meta name="description"         content="web development portfolio by nicolas fraisse, a developer based in montreal · get in touch at info@nicolasfraisse.com" />
         <meta property="og:url"          content="https://www.nicolasfraisse.com" />
         <meta property="og:title"        content="nicolas fraisse · creative freelance developer" />
-        <meta property="og:description"  content="web development portfolio by nicolas fraisse, a developer based in montreal · get in touch at info@nicolasfraisse.com" />
+        <meta property="og:description"  content="webby development portfolio by nicolas fraisse, a developer based in montreal · get in touch at info@nicolasfraisse.com" />
         <meta property="og:image"        content='https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Hexahedron.jpg/280px-Hexahedron.jpg'  />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="300" />
         <meta name="twitter:title"       content="nicolas fraisse · creative freelance developer" />
         <meta name="twitter:description" content="web development portfolio by nicolas fraisse, a developer based in montreal · get in touch at info@nicolasfraisse.com" />
         <meta name="twitter:image"       content='https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Hexahedron.jpg/280px-Hexahedron.jpg' />
