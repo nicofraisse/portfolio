88582f6ebd043426399e4c4e5b8c541ee50a6a7a
Nicolas Fraisse <ni.fraisse@gmail.com>
2020-06-24 16:19:28 -0400

    normal meta back

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,14 +19,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "Nicolas Fraisse",
-        short_name: "Nicolas Fraisse",
-        description: `Web development- test- portfolio for freelance work.`,
+        name: "nicolas fraisse · creative freelance developer",
+        short_name: "code portfolio",
+        description: `web development portfolio by nicolas fraisse, a developer based in montreal · get in touch at info@nicolasfraisse.com`,
         background_color: "#f7f3eb",
         theme_color: "#323442",
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        icon: "src/images/logo2.png", // This path is relative to the root of the site.
+        icon: "src/images/logo1.png", // This path is relative to the root of the site.
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import About from './about.js'
 import Contact from './contact.js'
 import ReactTooltip from "react-tooltip";
 import { Helmet } from "react-helmet";
-// import logo from '../images/logo.png'
+import socialLogo from '../images/social-logo.png'
 
 const Index = () => {
   const baseUrl = typeof window !== 'undefined' ? window.location.origin : 'https://www.nicolasfraisse.com';
@@ -18,20 +18,17 @@ const Index = () => {
     <div>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>code (Test) by nicolas fraisse · creative freelance developer</title>
-        <link rel="canonical" href="http://nicolasfraisse.com"/>
-        <meta name="description"         content="web TEST development portfolio by nicolas fraisse, a developer based in montreal · get in touch at info@nicolasfraisse.com" />
-        <meta property="og:url"          content="http://nicolasfraisse.com" />
-        <meta property="og:title"        content="nicolas fraisseFBTEST· creative freelance developer" />
-        <meta property="og:description"  content="webby development portfolio by nicolas fraisse, a developer based in montreal · get in touch at info@nicolasfraisse.com" />
-        <meta property="og:image"        content='https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Hexahedron.jpg/280px-Hexahedron.jpg?123'  />
-        <meta property="og:updated_time"        content='1593020458'  />
-        <meta property="og:image:type" content="image/jpg" />
-        <meta property="og:image:width" content="400" />
-        <meta property="og:image:height" content="300" />
-        <meta name="twitter:title"       content="nicolas fraisse TEST · creative freelance developer" />
+        <title>code by nicolas fraisse · creative freelance developer</title>
+        <link rel="canonical" href="https://www.nicolasfraisse.com"/>
+        <meta property="og:image"        content={`${baseUrl}${socialLogo}`}  />
+        <meta property="og:type"         content="website" />
+        <meta name="description"         content="web development portfolio by nicolas fraisse, a developer based in montreal · get in touch at info@nicolasfraisse.com" />
+        <meta property="og:url"          content="https://www.nicolasfraisse.com" />
+        <meta property="og:title"        content="nicolas fraisse · creative freelance developer" />
+        <meta property="og:description"  content="web development portfolio by nicolas fraisse, a developer based in montreal · get in touch at info@nicolasfraisse.com" />
+        <meta name="twitter:title"       content="nicolas fraisse · creative freelance developer" />
         <meta name="twitter:description" content="web development portfolio by nicolas fraisse, a developer based in montreal · get in touch at info@nicolasfraisse.com" />
-        <meta name="twitter:image"       content='https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Hexahedron.jpg/280px-Hexahedron.jpg?123' />
+        <meta name="twitter:image"       content={`${baseUrl}${socialLogo}`} />
         <meta name="twitter:card"        content="summary_large_image" />
       </Helmet>
       <Main>
