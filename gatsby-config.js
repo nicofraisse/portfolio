6e2b211943e49a117a786555b9afec056ebcd9c9
Nicolas Fraisse <ni.fraisse@gmail.com>
2020-06-24 19:02:18 -0400

    fixed some lighthous issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
         description: `web development portfolio by nicolas fraisse, a developer based in montreal · get in touch at info@nicolasfraisse.com`,
         background_color: "#f7f3eb",
         theme_color: "#323442",
+        lang: `en`,
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         icon: "src/images/logo.png", // This path is relative to the root of the site.
